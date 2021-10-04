9e42084e37325676a369bcfe943ca7ca0cc4a728
Sinq <56228085+zox004@users.noreply.github.com>
2021-10-04 23:47:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ def start_menu():
     print("1. 기존사용자")
     print("2. 신규사용자\n")
     num = int(input("번호를 입력해주세요 : "))
-    if(num != 1 or num != 2):
+    if num != 1 and num != 2:
         start_menu()
-    else:
-        clear()
-        return num
+    elif num == 1:
+        existing_user()
+    elif num == 2:
+        new_user()
 
-def existing_user(num):
-    if(num == 1):
-        id = input("ID : ")
-        pw = input("PW : ")
+        
+
+def existing_user():
+    id = input("ID : ")
+    pw = input("PW : ")
+
+def new_user():
+    pass
 
 start_menu()
