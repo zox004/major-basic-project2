a7b6bfd76a26ef0323be0497fddf36e40ba66d72
Sinq <56228085+zox004@users.noreply.github.com>
2021-10-08 00:52:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import os
+from time import sleep
+import new_user as nu
+import existing_user as eu
 
-def clear():
-    os.system('cls')
+clear = lambda : os.system('cls')
 
 def start_menu():
+    clear()
     print("1. 기존사용자")
     print("2. 신규사용자\n")
     num = int(input("번호를 입력해주세요 : "))
     if num != 1 and num != 2:
+        clear()
+        print("올바른 입력이 아닙니다. 다시 입력해주세요.")
+        sleep(2)
+        clear()
         start_menu()
     elif num == 1:
-        existing_user()
+        eu.existing_user()
     elif num == 2:
-        new_user()
-
-        
-
-def existing_user():
-    id = input("ID : ")
-    pw = input("PW : ")
-
-def new_user():
-    pass
+        nu.new_user()
 
 start_menu()
