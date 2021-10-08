7f4b5b0d038f991a73c50cf81ac7e073db8eccda
Sinq <56228085+zox004@users.noreply.github.com>
2021-10-08 10:44:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ def start_menu():
     clear()
     print("1. 기존사용자")
     print("2. 신규사용자\n")
-    num = int(input("번호를 입력해주세요 : "))
-    if num != 1 and num != 2:
+
+    num = input("번호를 입력해주세요 : ")
+
+    if num == '':
+        clear()
+        print("입력된 값이 없습니다. 다시 입력해주세요.")
+        sleep(2)
+        clear()
+        start_menu()
+    elif num == '1':
+        eu.existing_user()
+    elif num == '2':
+        nu.new_user()
+    else:
         clear()
         print("올바른 입력이 아닙니다. 다시 입력해주세요.")
         sleep(2)
         clear()
         start_menu()
-    elif num == 1:
-        eu.existing_user()
-    elif num == 2:
-        nu.new_user()
 
 start_menu()
