625a66e9dcfac3593188de8109aa97baa4308602
Sinq <56228085+zox004@users.noreply.github.com>
2021-10-14 18:12:52 +0900

    Add files via upload

---
+++ start_menu.py
@@ -0,0 +1,30 @@
+import os
+from time import sleep
+import new_user as nu
+import existing_user as eu
+
+clear = lambda : os.system('cls')
+
+def start_menu():
+    clear()
+    print("1. 기존사용자")
+    print("2. 신규사용자\n")
+
+    num = input("번호를 입력해주세요 : ")
+
+    if num == '':
+        clear()
+        print("입력된 값이 없습니다. 다시 입력해주세요.")
+        sleep(2)
+        clear()
+        start_menu()
+    elif num == '1':
+        eu.existing_user()
+    elif num == '2':
+        nu.new_user()
+    else:
+        clear()
+        print("올바른 입력이 아닙니다. 다시 입력해주세요.")
+        sleep(2)
+        clear()
+        start_menu()
