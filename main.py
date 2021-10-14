3642cd195f0dc4e479ed7e7c8d79c864d59cf451
Sinq <56228085+zox004@users.noreply.github.com>
2021-10-14 18:07:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import os
 from time import sleep
-import new_user as nu
-import existing_user as eu
 import start_menu as sm
+import new_user as nu
+import pandas as pd
 
-sm.start_menu()
+clear = lambda : os.system('cls')
+
+def integrity_test():
+    path_dir = 'Data'                #Data 디렉터리 
+    id_list = os.listdir(path_dir)   #Data 디렉터리에 있는 id.txt들
+    if id_list:
+        clear()
+        for line in id_list:
+            position = line.find('.')
+            if not (4<=position<=10 or line[position+1:]=='txt'):
+                print("규칙에 위배되는 데이터가 존재합니다.")
+                sleep(2)
+                exit()
+            with open("Data\\{}".format(line), 'r', encoding='utf8') as user_info:
+                for line in user_info.readlines()[:3]:
+                    pw = line[0]
+                    hint_question = line[1]
+                    hint_answer = line[2]
+                for i in pw:
+                    if not (48<=ord(i)<=57 or 65<=ord(i)<=90 or 97<=ord(i)<=122 or 4<len(pw)<10):
+                        clear()
+                        print("규칙에 위배되는 데이터가 존재합니다.")
+                        sleep(2)
+                        exit()
+                if not (len(hint_question) <= 50 or len(hint_answer) <= 50):
+                        clear()
+                        print("규칙에 위배되는 데이터가 존재합니다.")
+                        sleep(2)
+                        exit()
+                    
+    else:
+        clear()
+        print("기존사용자가 등록되어 있지 않습니다.")
+        print("신규사용자를 등록해주세요.")
+        sleep(2)
+        clear()
+        nu.new_user()
+        pass
+integrity_test()
