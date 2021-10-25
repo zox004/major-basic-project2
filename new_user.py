4f417e13820dbc1ee0a9caa7cbb8111a76b05d2d
Sinq <56228085+zox004@users.noreply.github.com>
2021-10-25 21:42:56 +0900

    Update new_user.py

--- new_user.py
+++ new_user.py
@@ -1,249 +1,104 @@
 import os
 clear = lambda : os.system('cls')
-from time import sleep 
-import main_menu as mm
+from time import sleep
+import start_menu as sm
 
-def new_password(id) :
-    clear()
-    while True :
-        print("새로운 사이트의 DNS(이름)를 입력해주세요.")
-        DNS = input("DNS : ")
-        if(DNS==""):
-            clear()
-            print("입력된 값이 없습니다. 다시 입력해주세요.")
-            sleep(2)
-            clear()
-            continue
-        elif (DNS.count(" ")>0):
-            clear()
-            print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-            sleep(2)
-            clear()
-            continue
-        else:
-            clear()
-            break
+def new_user():
+    while(True):
         
+        print("<조건>\nID 및 PW는 4~10자로 입력하시길 바랍니다.\n")
+        new_id = input("New ID : ")
 
-    print("새로운 사이트의 PW조건을 입력해주세요.")
-
-    password_conditions = ["N", "N", "N", "N", 0] # PW 조건 디폴트 값 
-    print("PW 에 콤마는 들어갈 수 없습니다.")
-
-    while True :
-        special_character = input("PW 조건(특수문자 여부 -> Y/N) : ")
-        if(special_character==""):
-            print("입력된 값이 없습니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-        if(special_character == "Y" or special_character == "N"):
-            if(special_character == "Y"):
-                password_conditions[0] = "Y"
-                break
-            else :
-                break
-        else :
-            print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-
-    while True :
-        number = input("PW 조건(숫자 여부 -> Y/N) : ")
-        if(number==""):
-            print("입력된 값이 없습니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-        if(number == "Y" or number == "N"):
-            if(number == "Y"):
-                password_conditions[1] = "Y"
-                break
-            else :
-                break
-        else :
-            print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-
-    while True :
-        big_letter = input("PW 조건(대문자 여부 -> Y/N) : ")
-        if(big_letter==""):
-            print("입력된 값이 없습니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-        if(big_letter == "Y" or big_letter == "N"):
-            if(big_letter == "Y"):
-                password_conditions[2] = "Y"
-                break
-            else :
-                break
-        else :
-            print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-
-    while True :
-        small_letter = input("PW 조건(소문자 여부 -> Y/N) : ")
-        if(small_letter==""):
-            print("입력된 값이 없습니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-        if(small_letter == "Y" or small_letter == "N"):
-            if(small_letter == "Y"):
-                password_conditions[3] = "Y"
-                break
-            else :
-                break
-        else :
-            print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-
-    while True :
-        try:
-            minimum_number = input("PW 조건(최소자릿수-숫자) : ")
-            minimum_number = int(minimum_number)
-            password_conditions[4] = minimum_number
-            break
-        except ValueError:
-            if minimum_number == "" : # 입력받은 값이 없을 경우 ..
+        #============ ID 문법 규칙 검사 ==============
+        for i in new_id:
+            if (not 48<=ord(i)<=57) and (not 65<=ord(i)<=90) and (not 97<=ord(i)<=122) :
                 clear()
-                print("입력된 값이 없습니다. 다시 입력해주세요.")
+                print("조건에 맞지 않거나 틀린 형식입니다.")
+                print("다시 입력 하세요.\n")
                 sleep(2)
+                new_user()
+        #===========================================
+
+
+        #============ 기존 사용자 검색 ==============
+        path = "Data"
+        userinfo_list = os.listdir(path) # 해당 디렉토리의 모든 파일을 리스트로 받음
+        #===========================================
+
+        if ("{}.txt".format(new_id) in userinfo_list): # 리스트의 특정 파일 검색 (기존ID)
+            clear()
+            print("이미 가입된 ID 입니다.\n초기 화면으로 돌아갑니다.")
+            sleep(2)
+            continue
+
+        elif(len(new_id)<4 or len(new_id)>10):
+            clear()
+            print("조건에 맞지 않거나 틀린 형식입니다.")
+            print("다시 입력 하세요.\n")
+            sleep(2)
+            continue
+
+        else: # 모든 오류 통과 시 파일 생성 및 저장 
+            new_info = open('{}.txt'.format(new_id), "w", encoding="utf8")
+            new_info.close()
+
+        new_pw = input("New PW : ")
+
+        #============ PW 문법 규칙 검사 ==============
+        for i in new_pw:
+            if (not 48<=ord(i)<=57) and (not 65<=ord(i)<=90) and (not 97<=ord(i)<=122) :
                 clear()
-                continue
-            else: # 공백처리 문자열처리 .. 
-                clear()
-                print("올바른 입력이 아닙니다. 다시 입력해주세요.")
+                print("조건에 맞지 않거나 틀린 형식입니다.")
+                print("다시 입력 하세요.\n")
                 sleep(2)
-                clear()
-                continue
+                new_user()
+        #===========================================
 
-    user_info = open("Data\\{}.txt".format(id), "a", encoding='utf8')
-
-
-    while True :
-        clear()
-        print("<새로운 Password 등록>\n")
-        print("사이트명 : {}".format(DNS))
-        site_id = input("ID : ")
-        if (site_id == ""):
-            print("입력된 값이 없습니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-        elif(site_id.count(" ")>0):
-            print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-            continue
-        elif(not site_id.isalnum()):
-            print("특수문자를 포함하고 있습니다.")
+        if(len(new_pw)<4 or len(new_pw)>10):
+            clear()
+            print("조건에 맞지 않거나 틀린 형식입니다.")
+            print("다시 입력 하세요.\n")
+            os.remove("{}.txt".format(new_id))
             sleep(2)
             continue
         else:
-            break
+            new_info = open("{}.txt".format(new_id), "a", encoding="utf8")
+            print("{}".format(new_pw), file=new_info)
 
-    while True :
-        clear() 
 
-        site_password = input("PW : ")
-
-        if (site_password ==""):
-            print("입력된 값이 없습니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-        elif (site_password.count(" ")>0):
-            print("올바른 입력이 아닙니다. 다시 입력해주세요")
-            sleep(2)
-            continue
-
-        # 특수문자 검사 
-        if(password_conditions[0]=="Y"):
-            if(site_password.isalnum()):
-                print("올바른 입력이 아닙니다. 다시 입력해주세요")
+        while True:
+            hint_question = input("힌트 질문 : ")
+            if str.isspace(hint_question) :
+                print("조건에 맞지 않거나 틀린 형식입니다.")
+                print("다시 입력 하세요.\n")
                 sleep(2)
                 continue
-        else:
-            if(not site_password.isalnum()):
-                print("올바른 입력이 아닙니다. 다시 입력해주세요")
+            else :
+                print("{}".format(hint_question), file=new_info)
+            hint_answer = input("힌트 답 : ")
+            if str.isspace(hint_answer) :
+                print("조건에 맞지 않거나 틀린 형식입니다.")
+                print("다시 입력 하세요.\n")
                 sleep(2)
                 continue
-        
-        # 숫자 검사
-        number_count = 0
-        if(password_conditions[1]=="Y"):
-            for i in site_password: 
-                if 48<=ord(i)<=57 :
-                    number_count += 1
-            if number_count == 0 :
-                print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-                sleep(2)
-                continue                
-        else:
-            for i in site_password: 
-                if 48<=ord(i)<=57 :
-                    number_count += 1
-            if number_count > 0 :
-                print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-                sleep(2)
-                continue
+            else :
+                print("{}".format(hint_answer), file=new_info)
+                break
 
+        new_info.close()
 
-        # 대문자 검사
-        big_count = 0
-        if(password_conditions[2]=="Y"):
-            for i in site_password: 
-                if 65<=ord(i)<=90 :
-                    big_count += 1
-            if big_count == 0 :
-                print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-                sleep(2)
-                continue                
-        else:
-            for i in site_password: 
-                if 65<=ord(i)<=90 :
-                    big_count += 1
-            if big_count > 0 :
-                print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-                sleep(2)
-                continue
-        
-        # 소문자 검사
-        small_count = 0
-        if(password_conditions[3]=="Y"):
-            for i in site_password: 
-                if 97<=ord(i)<=122 :
-                    small_count += 1
-            if small_count == 0 :
-                print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-                sleep(2)
-                continue
-        else:
-            for i in site_password: 
-                if 97<=ord(i)<=122 :
-                    small_count += 1
-            if small_count > 0 :
-                print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-                sleep(2)
-                continue
-        
-        # 최소 자릿수 검사 
-        if(len(site_password) < password_conditions[4]) :
-            print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-            sleep(2)
-            continue
-
+        # 최종적으로 저장된 ID 텍스트 파일은 특정 지정폴더로 이동합니다
+        # src : 이동 전 디렉토리 , dir : 이동 후 디렉토리
+        # 해당 디렉토리를 사용자 워크스페이스에 맞춰서 변경해야 합니다.
+        #================ 파일 경로 변경 ================
+        import shutil 
+        filename = '{}.txt'.format(new_id) 
+        src = 'C:\\Users\\sinkyu\\Desktop\\PythonWorkspace\\basic_project\\' 
+        dir = 'C:\\Users\\sinkyu\\Desktop\\PythonWorkspace\\basic_project\\Data\\' 
+        shutil.move(src + filename, dir + filename)
+        #===============================================
 
         break
 
-    user_info.write("{},".format(DNS))
-    user_info.write("{},".format(site_id))
-    user_info.write("{},".format(site_password))
-    # password_conditions_string = ','.join(password_conditions)
-    # user_info.write("{}".format(password_conditions))
-    for index, i in enumerate(password_conditions):
-        if index == 4:
-            user_info.write("{}".format(i))
-            break
-        user_info.write("{},".format(i))
-    print("", file=user_info)
-
-    user_info.close()
-    mm.main_menu(id)
+    clear()
+    sm.start_menu()
