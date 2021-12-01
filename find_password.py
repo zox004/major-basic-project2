6f3484056730449fc0365714f84277bd58aa954c
Sinq <56228085+zox004@users.noreply.github.com>
2021-12-01 21:23:20 +0900

    Update find_password.py

--- find_password.py
+++ find_password.py
@@ -6,67 +6,74 @@ from msvcrt import getch
 
 clear = lambda : os.system('cls')
 
-def find_password(id):
+def find_password(user_id):
     while True:
         print('저장된 사이트 리스트')
-        with open("Data\\"+id+".txt", "r", encoding='utf8') as user_info:
+        with open("Data\\"+user_id+".txt", "r", encoding='utf8') as user_info:
             dns = []
             id_list = []
             pw_list = []
-            condition1 = []
-            condition2 = []
-            condition3 = []
-            condition4 = []
-            condition5 = []
             row_len = 0
             cnt = 1
 
             for line in user_info.readlines()[3:]:
                 line = line.strip()
-                user_info_row = line.split(',')
+                user_info_row = line.split('\t')
                 dns.append(user_info_row[0])
                 id_list.append(user_info_row[1])
                 pw_list.append(user_info_row[2])
-                condition1.append(user_info_row[3])
-                condition2.append(user_info_row[4])
-                condition3.append(user_info_row[5])
-                condition4.append(user_info_row[6])
-                condition5.append(user_info_row[7])
                 row_len = cnt
                 cnt = cnt + 1
 
-            df_user_info = pd.DataFrame({'사이트':dns, 'ID':id_list, 'PW':pw_list,
-                                        'condition1':condition1, 'condition2':condition2, 'condition3':condition3,
-                                        'condition4':condition4, 'condition5':condition5})
+            df_user_info = pd.DataFrame({'사이트':dns, 'ID':id_list, 'PW':pw_list})
             if row_len == 0:        #저장된 사이트가 없을 시 임시 코드
                 clear()
                 print('저장된 사이트가 없습니다.')
                 print('새로운 사이트를 추가해주세요.')
                 sleep(2)
-                mm.main_menu(id)
+                mm.main_menu(user_id)
                 
             for i in range(row_len):
                 print('{}. {}'.format(i+1, df_user_info['사이트'][i]))
             try:
-                num = input('\nPW를 확인하고자 하는 사이트의 번호를 입력해주세요: ')
-                num = int(num)
-                if 0 < num <= row_len:
-                    clear()
-                    print("ID는 {}".format(df_user_info['ID'][num-1]))
-                    print("PW는 {}".format(df_user_info['PW'][num-1]))
-                    print("입니다.")
-                    print("아무키나 누르면 메인화면으로 돌아갑니다.")
-                    getch()
-                    clear()
-                    mm.main_menu(id)
+                site = input('\nPW를 확인하고자 하는 사이트의 번호 또는 DNS를 입력해주세요: ')
+                if site.isnumeric():
+                    site = int(site)
+                    if 0 < site <= row_len:
+                        clear()
+                        print("ID는 {}".format(df_user_info['ID'][site-1]))
+                        print("PW는 {}".format(df_user_info['PW'][site-1]))
+                        print("입니다.")
+                        print("아무키나 누르면 메인화면으로 돌아갑니다.")
+                        getch()
+                        clear()
+                        mm.main_menu(user_id)
+                    else:
+                        clear()
+                        print("올바른 입력이 아닙니다. 다시 입력해주세요.")
+                        sleep(2)
+                        clear()
+                        continue
                 else:
                     clear()
-                    print("올바른 입력이 아닙니다. 다시 입력해주세요.")
-                    sleep(2)
-                    clear()
-                    continue
+                    for i in range(row_len):
+                        if site == df_user_info['사이트'][i]:
+                            print("ID는 {}".format(df_user_info['ID'][i]))
+                            print("PW는 {}".format(df_user_info['PW'][i]))
+                            print("입니다.")
+                            print("아무키나 누르면 메인화면으로 돌아갑니다.")
+                            getch()
+                            clear()
+                            mm.main_menu(user_id)
+                    else:
+                        clear()
+                        print("올바른 입력이 아닙니다. 다시 입력해주세요.")
+                        sleep(2)
+                        clear()
+                        continue
+                    
             except ValueError:
-                if num == "":
+                if site == "":
                     clear()
                     print("입력된 값이 없습니다. 다시 입력해주세요.")
                     sleep(2)
