b4c468e7d607a403d88670775658cfbbe83c7c32
Sinq <56228085+zox004@users.noreply.github.com>
2021-12-01 14:02:59 +0900

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -3,6 +3,7 @@ from time import sleep
 import new_password as np
 import find_password as fp
 import change_password as cp
+import remove_password as rm
 
 clear = lambda : os.system('cls')
 
@@ -11,7 +12,8 @@ def main_menu(user_exiting_id):
     print("1. 기존 PW 찾기")
     print("2. 기존 PW 변경")
     print("3. 새로운 PW 추가")
-    print("4. 시스템 종료\n")
+    print("4. PW 삭제")
+    print("5. 시스템 종료\n")
     num = input("번호를 입력하여, 메뉴를 선택해주세요:")
     if num == '':
         clear()
@@ -29,6 +31,28 @@ def main_menu(user_exiting_id):
         clear()
         np.new_password(user_exiting_id)
     elif num == '4':
+        rm.remove_password(user_exiting_id)
+    elif num == '5':
+        #암호화
+        encoding_line = ""
+        with open("Data\\{}.txt".format(user_exiting_id), "r", encoding='utf8') as user_info:
+            for line in user_info.readlines():
+                for i in line:
+                    if i == '\n':
+                        encoding_line += '\n'
+                        continue
+                    elif i == '\t':
+                        encoding_line += '\t'
+                        continue
+                    elif 65 <= ord(i) <= 122:
+                        tmp = ord(i)-30
+                    elif 33 <= ord(i) <= 64:
+                        tmp = ord(i)-30
+                    else:
+                        tmp = ord(i)-100
+                    encoding_line += chr(tmp)
+        with open("Data\\{}.txt".format(user_exiting_id), "w", encoding='utf8') as user_info:
+            user_info.write(encoding_line)
         exit()
     else:
         clear()
